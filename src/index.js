b498cd0efd9582790aa928754eccd6a495e5fb33
John Gomez <johncar@gmail.com>
2019-01-17 16:19:50 -0500

    Here we go

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider, Box, Alert } from 'rendition';
+
+ReactDOM.render(
+  <Provider>
+    <Box m={3}>
+        <Alert my={2} success>
+          Let's get started!
+        </Alert>
+    </Box>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
