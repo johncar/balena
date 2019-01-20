693c441929a5bb1bf92f5e77924278427f0e956d
John Gomez <johncar@gmail.com>
2019-01-19 19:11:47 -0500

    Base React components

---
+++ src/App/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './../Header';
+import Toolbar from './../Toolbar';
+import Lightning from './../Lightning';
+import { Alert } from 'rendition';
+
+const API = 'http://localhost:3000/api/v1/';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lightning: undefined,
+            error: null
+        };
+    }
+
+    componentDidMount() {
+        this.setState({});
+
+        fetch(API + 'device')
+          .then(response => response.json())
+          .then(result => this.setState({ lightning: result.data }))
+          .catch(error => this.setState({ error }));;
+    }
+
+    render() {
+        const { lightning, error } = this.state;
+
+        if (error) {
+            return (
+                <div>
+                    <Header/>
+                    <Toolbar/>
+                    <Alert danger>{error.message}</Alert>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <Header/>
+                <Toolbar/>
+                <Lightning data={lightning}/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.scss';
+import {Flex, Txt, DropDownButton } from 'rendition';
+import Moment from 'moment';
+
+const Header = () => (
+    <Flex justify="space-between" className={'Header'}>
+      <Txt className={'Header_txt'}>{Moment(new Date()).format('dddd D MMMM, YYYY')}</Txt>
+      <Txt className={'Header_txt'}>{Moment(new Date()).format('H:mm A')}</Txt>
+      <Flex className={'Header_user'} justify="space-between">
+        <Txt className={'Header_txt'}>John Gomez</Txt>
+        <Txt className={'Header_txtCircle'}>JG</Txt>
+        <DropDownButton mx={0} joined tertiary className={'Header_user_menu'} >
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+         </DropDownButton>
+      </Flex>
+    </Flex>
+);
+
+export default Header;
