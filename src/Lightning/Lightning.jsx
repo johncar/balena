9fb5f50415e565fb5a20ef55b19eabc5d52d2627
John Gomez <johncar@gmail.com>
2019-01-20 22:38:50 -0500

    Working MVP

--- src/Lightning/Lightning.jsx
+++ src/Lightning/Lightning.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Flex, Box, Table, Txt, ArcSlider } from 'rendition';
-import _cloneDeep from 'lodash/cloneDeep';
 
 import './Lightning.scss';
 
@@ -9,27 +8,27 @@ import * as FaToggleOff from 'react-icons/lib/fa/toggle-off';
 import * as TiStarburstOutline from 'react-icons/lib/ti/starburst-outline';
 
 const columns = [
-  {
-    field: 'name',
-    label: 'Room',
-    sortable: true,
-  },
-  {
-    field: 'active',
-    label: 'State',
-    sortable: false,
-    render: value => {
-        return (value) ?
+    {
+        field: 'name',
+        label: 'Room',
+        sortable: true,
+    },
+    {
+        field: 'active',
+        label: 'State',
+        sortable: false,
+        render: value => {
+            return (value) ?
             (<Txt><FaToggleOn color="#4DB313"/> On</Txt>) :
             (<Txt><FaToggleOff/> Off</Txt>);
+        }
+    },
+    {
+        field: 'brightness',
+        label: 'Brightness',
+        sortable: false,
+        render: value => value + '%'
     }
-  },
-  {
-    field: 'brightness',
-    label: 'Brightness',
-    sortable: false,
-    render: value => value + '%'
-  }
 ]
 
 class Lightning extends React.Component {
@@ -39,10 +38,12 @@ class Lightning extends React.Component {
         this.state = {
             selectedLight: undefined,
             data: [],
+            sliderValue: 0
         }
 
         this.rowClicked = (row, event) => {
-            this.setState({ selectedLight: row });
+            const sliderValue = row.brightness /100;
+            this.setState({ 'selectedLight': row, 'sliderValue': sliderValue });
         }
 
         this.lightHandler = sliderValue => {
@@ -55,6 +56,7 @@ class Lightning extends React.Component {
                 changes['brightness'] = 0;
             }
             this.changeData( this.state.selectedLight, changes);
+            this.setState({ 'sliderValue': sliderValue });
         }
 
         this.changeData = (item, change) => {
@@ -62,7 +64,6 @@ class Lightning extends React.Component {
             for (let i = 0, len = newData.length; i < len; i++) {
                 if (item !== undefined) {
                     if (newData[i].id === item.id) {
-                        //light.brightness = sliderValue * 100;
                         newData[i] = Object.assign({}, newData[i], change);
                     }
                 }
@@ -83,7 +84,6 @@ class Lightning extends React.Component {
     }
 
     render() {
-
         if (this.state.selectedLight === undefined) {
             return (
                 <Box m={2} className={'Lightning_table'}>
@@ -104,42 +104,18 @@ class Lightning extends React.Component {
                 onRowClick={this.rowClicked}/>
             </Box>
             <Box m={2} bg={'#4D5057'} className={'Lightning_slider'}>
-              <Slider lightHandler={this.lightHandler} value={ this.state.selectedLight.brightness / 100 }/>
+              <ArcSlider value={this.state.sliderValue} onValueChange={this.lightHandler}
+                  className={'Lightning_arc_slider'}>
+                  <p><TiStarburstOutline/></p>
+                  <h2>{Math.round(this.state.sliderValue * 100)}%</h2>
+                  <p>Brightness</p>
+              </ArcSlider>
             </Box>
         </Flex>);
     }
 
 }
 
-class Slider extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      value: 0
-    }
-
-    this.handleChange = value => {
-      this.setState({ value });
-      this.props.lightHandler(value);
-    }
-  }
-
-  static getDerivedStateFromProps(prevProps, prevState) {
-      const value = prevProps.value !== undefined ? prevProps.value : 0;
-      return {'value': value};
-  }
-
-  render () {
-      return (
-          <ArcSlider background='#c6c8c9' value={this.state.value} onValueChange={this.handleChange}>
-            <p><TiStarburstOutline/></p>
-            <h2>{Math.round(this.state.value * 100)}%</h2>
-            <p>Brightness</p>
-          </ArcSlider>);
-
-  }
-}
 
 
 export default Lightning;
